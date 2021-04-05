aeff7b68e7a2b2936dc37d54c452f3492618c4cd
sean <spcavanaugh88@gmail.com>
2021-04-05 10:36:11 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_1_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int ageF = int.Parse(age);
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name + " your age is " + ageF + " your favorite color is ," + color);
+            
+            
+        }
+    }
+}
